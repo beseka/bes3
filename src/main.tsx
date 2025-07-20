e7216ff9d71472934aa743ecdebd01152e2835a7
beseka <bsirhatk@gmail.com>
2025-07-20 18:40:48 +0300

    refactor: switch from BrowserRouter to HashRouter for routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 import SplashScreen from './pages/splashscreen';
-import './index.css';
 import DetailsPage from './pages/details';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<SplashScreen />} />
         <Route path="/details" element={<DetailsPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
